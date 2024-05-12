0db2a1d67be9556fdb15d605e1fe1af825005675
Laroxyss <lomonosov_18@mail.ru>
2024-05-12 14:30:49 +0500

    Переделал Edge через From-To, исправил ошибку ненужного добавления ребра в from.edges в методе IsConnected

--- MySnake/Model/MapGenerator/Graph.cs
+++ MySnake/Model/MapGenerator/Graph.cs
@@ -28,25 +28,38 @@ public class Graph<T>
         edge.DeleteEdge();
     }
 
-    public void RemoveEdge(Node<T> node1, Node<T> node2)
+    public void RemoveEdge(Node<T> from, Node<T> to)
     {
-        if (!Nodes.Contains(node1) || !Nodes.Contains(node2))
-            throw new Exception("The vertices are not in the graph");
+        if (!Nodes.Contains(from) || !Nodes.Contains(to))
+            throw new Exception("The nodes are not in the graph");
 
-        RemoveEdge(new Edge<T>(node1, node2));
+        RemoveEdge(new Edge<T>(from, to));
+    }
+
+    public void RemoveEdgeBidirectionally(Node<T> node1, Node<T> node2)
+    {
+        if (IsConnected(node1, node2))
+            RemoveEdge(node1, node2);
+        if (IsConnected(node2, node1))
+            RemoveEdge(node2, node1);
     }
 
     public void RemoveNode(Node<T> node)
     {
         foreach (var edge in node.Edges)
-            RemoveEdge(edge);
+            RemoveEdgeBidirectionally(edge.From, edge.To);
 
         Nodes.Remove(node);
     }
 
-    public bool IsConnected(Node<T> node1, Node<T> node2)
+    public bool IsConnected(Node<T> from, Node<T> to)
     {
-        return Edges.Contains(new Edge<T>(node1, node2));
+        return Edges.Contains(new Edge<T>(from, to));
+    }
+
+    public bool IsConnectedBidirectionally(Node<T> node1, Node<T> node2)
+    {
+        return IsConnected(node1, node2) && IsConnected(node2, node1);
     }
 
     public IEnumerable<Node<T>> GetNeighbors(Node<T> node)
@@ -55,84 +68,105 @@ public class Graph<T>
             throw new Exception("Node is not contained in the graph");
 
         foreach (var edge in node.Edges)
-            yield return edge.Node1.Equals(node) ? edge.Node2 : edge.Node1;
+            yield return edge.To;
     }
 
-    public bool Connect(Node<T> node1, Node<T> node2)
+    public bool Connect(Node<T> from, Node<T> to)
     {
-        if (node1.Equals(node2))
+        if (from.Equals(to))
             throw new Exception("Node cannot be connected to itself");
-        if (!Nodes.Contains(node1) || !Nodes.Contains(node2))
+        if (!Nodes.Contains(from) || !Nodes.Contains(to))
             throw new Exception("The nodes are not in the graph");
 
-        return Edges.Add(new Edge<T>(node1, node2));
+        var edge = new Edge<T>(from, to);
+        edge.Save();
+        return Edges.Add(edge);
+    }
+
+    public void ConnectBidirectionally(Node<T> node1, Node<T> node2)
+    {
+        Connect(node1, node2);
+        Connect(node2, node1);
     }
 
     public void ConnectAllNodes()
     {
         foreach (var node1 in Nodes)
-        {
-            foreach (var node2 in Nodes)
-            {
-                if (node1 != node2)
-                    Connect(node1, node2);
-            }
-        }
+        foreach (var node2 in Nodes.Where(node2 => node1 != node2))
+            ConnectBidirectionally(node1, node2);
     }
 }
 
 public class Node<T>
 {
     public T Value { get; set; }
-    public readonly HashSet<Edge<T>> Edges = new();
+    internal readonly HashSet<Edge<T>> Edges = new();
 
     public Node(T value)
     {
         Value = value;
     }
 
-    public bool RemoveEdge(Edge<T> edge)
+    internal void RemoveEdge(Edge<T> edge)
     {
-        return Edges.Remove(edge);
+        if (!Edges.Remove(edge))
+            throw new Exception("Node does not contain an edge");
     }
 }
 
 public class Edge<T>
 {
-    public Node<T> Node1 { get; }
-    public Node<T> Node2 { get; }
+    public Node<T> From { get; }
+    public Node<T> To { get; }
 
-    public Edge(Node<T> node1, Node<T> node2)
+    public Edge(Node<T> from, Node<T> to)
     {
-        Node1 = node1;
-        Node2 = node2;
-        Node1.Edges.Add(this);
-        Node2.Edges.Add(this);
+        From = from;
+        To = to;
     }
 
-    public void DeleteEdge()
+    internal void Save()
     {
-        if (!Node1.RemoveEdge(this) || !Node2.RemoveEdge(this))
-            throw new Exception("Nodes do not contain an edge");
+        From.Edges.Add(this);
+    }
+
+    internal void DeleteEdge()
+    {
+        From.RemoveEdge(this);
     }
 
     public bool Equals(Edge<T> edge)
     {
-        return edge.Node1.Equals(this.Node1) && edge.Node2.Equals(this.Node2) ||
-               edge.Node2.Equals(this.Node1) && edge.Node1.Equals(this.Node2);
+        return From.Equals(edge.From) && To.Equals(edge.To);
+    }
+    
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+            return false;
+
+        var edge = (Edge<T>) obj;
+        return From.Equals(edge.From) && To.Equals(edge.To);
+    }
+
+    public static bool operator ==(Edge<T> edge1, Edge<T> edge2)
+    {
+        return edge1.Equals(edge2);
+    }
+
+    public static bool operator !=(Edge<T> edge1, Edge<T> edge2)
+    {
+        return !edge1.Equals(edge2);
     }
 
     public override int GetHashCode()
     {
         unchecked
         {
-            var node1Hash = Node1.GetHashCode();
-            var node2Hash = Node2.GetHashCode();
-
             var hash = 17;
 
-            hash = hash * 31 + Math.Min(node1Hash, node2Hash);
-            hash = hash * 31 + Math.Max(node1Hash, node2Hash);
+            hash = hash * 31 + From.GetHashCode();
+            hash = hash * 31 + To.GetHashCode();
 
             return hash;
         }
